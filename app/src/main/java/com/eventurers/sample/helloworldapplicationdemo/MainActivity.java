cceb2ba0eb3ba1c4853ffcf194e5e331ada92802
ImmudiEventurers <immudi.eventurers@gmail.com>
2015-09-18 17:46:12 +0530

    Forked source changed

--- app/src/main/java/com/eventurers/sample/helloworldapplicationdemo/MainActivity.java
+++ app/src/main/java/com/eventurers/sample/helloworldapplicationdemo/MainActivity.java
@@ -10,6 +10,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        //added fork source comments
         setContentView(R.layout.activity_main);
     }
 
